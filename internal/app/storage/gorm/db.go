617d3faa5c63e93c07311c9db1d270caa7c658d0
NicoEberlein <nico@eberln.de>
2025-05-08 08:14:46 +0500

    ci: app now retries db connection before crashing

--- internal/app/storage/gorm/db.go
+++ internal/app/storage/gorm/db.go
@@ -1,6 +1,7 @@
 package gormstore
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"time"
@@ -8,10 +9,24 @@ import (
 
 func GetDbConnection() *gorm.DB {
 
-	dsn := "host=localhost user=notsamsa password=notsamsapw dbname=notsamsa port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
+	var db *gorm.DB
+	var err error
+
+	retryCounter := 0
+
+	for retryCounter < 3 {
+		dsn := "host=db user=notsamsa password=notsamsapw dbname=notsamsa port=5432 sslmode=disable"
+		fmt.Println("Attempting to connect to database. Retrycounter: ", retryCounter)
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		} else {
+			retryCounter++
+			if retryCounter == 3 {
+				panic(fmt.Errorf("failed to connect to database %s", err))
+			}
+			time.Sleep(8 * time.Second)
+		}
 	}
 
 	sqlDb, _ := db.DB()
